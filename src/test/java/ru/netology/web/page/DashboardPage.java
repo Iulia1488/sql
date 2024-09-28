dc672a96e67667f8ceae8200052454ef10f38586
Julia Bukova <ul4ik17@gmail.com>
2024-09-28 13:26:13 +0300

    commit

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,20 @@
+package ru.netology.web.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class DashboardPage {
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+    }
+
+}
